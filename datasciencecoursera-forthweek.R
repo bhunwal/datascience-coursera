7c8e5d70475c82365e6865af1ce51b2db0f07ab7
bhunwal <maneesh.bhunwal@gmail.com>
2017-12-06 05:34:39 +0530

    Update datasciencecoursera-forthweek.R

--- datasciencecoursera-forthweek.R
+++ datasciencecoursera-forthweek.R
@@ -72,3 +72,54 @@ rankhospital <- function (state, outcome, num) {
   }
   ret
 }
+
+
+rankall <- function(outcome, num = "best") {
+    ## Read outcome data
+     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+     
+     sorted <- NULL
+     col_name <- NULL
+     if(identical(outcome, "heart attack")) {
+       col_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+     }else if(identical(outcome, "heart failure")) {
+       col_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+     }else if(identical(outcome, "pneumonia")) {
+       col_name <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+     }else {
+       stop('invalid outcome')
+     }
+     
+    newdata2 <- subset(data, select=c(col_name, "Hospital.Name", "State"))
+    #convert to number
+    newdata2[,1] <- suppressWarnings(as.numeric(as.character(newdata2[,1])))
+    #remove nas
+    newdata2 <- na.omit(newdata2)
+    
+    #Sortby state, rankingm hos name
+    newdata2 <- newdata2[order(newdata2[,3], newdata2[,1], newdata2[,2]),]
+    statelist <- sort(unique(data[,'State']))
+    statehldr <- c()
+    hosthldr <- c()
+    for(stateitr in statelist) {
+      print(stateitr)
+      temp <- subset(newdata2,State == stateitr, select=c("Hospital.Name", "State"))
+      index <- num
+      if(identical(num, "best")) {
+        index <- 1;
+      }
+      else if(identical(num, "worst")) {
+        index <- nrow(temp);
+      }
+      ret <- NA
+      if(nrow(temp) >= index) {
+        ret <- temp[index,1]
+      }
+      
+      statehldr <- c(statehldr, stateitr)
+      hosthldr <- c(hosthldr, ret)
+      
+    }
+    df = data.frame(statehldr, hosthldr)  
+    df
+}
